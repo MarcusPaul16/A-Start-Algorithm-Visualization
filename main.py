898eb7179a345fa4b7428f3680561c6f12d1fa70
paulmarcus <ProgramatorLife1623>
2022-07-27 10:57:24 -0700

    Done working on grid drawing. Try to fix the end and start setters bug.

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ width = 800
 window = pygame.display.set_mode((width, width))
 
 
-class Cube:
+class Square:
     def __init__(self, row, column, width, rows):
         self.row = row
         self.column = column
@@ -51,6 +51,9 @@ class Cube:
     def reset(self):
         self.color = COLORS['WHITE']
 
+    def setStart(self):
+        self.color = COLORS['ORANGE']
+
     def setVisited(self):
         self.color = COLORS['RED']
 
@@ -65,3 +68,99 @@ class Cube:
 
     def setEnd(self):
         self.color = COLORS['PURPLE']
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.width))
+
+    def update_neighbors(self, grid):
+        pass
+
+    def __lt__(self, other):
+        return False
+
+
+def hFunction(h1, h2):
+    x1, y1 = h1
+    x2, y2 = h2
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def build_grid(rows, width):
+    grid = []
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            square = Square(i, j, gap, rows)
+            grid[i].append(square)
+
+    return grid
+
+def build_grid_lines(window, rows, width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(window, COLORS['GREY'], (0, i * gap), (width, i * gap))
+        for j in range(rows):
+            pygame.draw.line(window, COLORS['GREY'], (j * gap, 0), (j * gap, width))
+
+def draw(window, grid, rows, width):
+    window.fill(COLORS['WHITE'])
+    for row in grid:
+        for square in row:
+            square.draw(window)
+
+    build_grid_lines(window, rows, width)
+    pygame.display.update()
+
+def getPositionByMouse(position, rows, width):
+    gap = width // rows
+    x, y = position
+
+    row = y // gap
+    column = x // gap
+
+    return row, column
+
+
+def main(window, width):
+    rows = 50
+    grid = build_grid(rows, width)
+
+    startPosition = None
+    endPosition = None
+
+    run = True
+    started = True
+
+    draw(window, grid, rows, width)
+    while run:
+        draw(window, grid, rows, width)
+        for event in pygame.event.get():
+            print(event.type)
+            if event.type == pygame.QUIT:
+                run = False
+            if started:
+                continue
+            if pygame.mouse.get_pressed()[0]:
+                print("merge")
+                position = pygame.mouse.get_pos()
+                row, column = getPositionByMouse(position, rows, width)
+                square = grid[row][column]
+                if not startPosition:
+                    startPosition = square
+                    startPosition.setStart()
+                    print(3)
+                elif not endPosition:
+                    endPosition = square
+                    endPosition.setEnd()
+                    print(3)
+
+                elif square != startPosition and square != endPosition:
+                    square.setObstacle()
+
+            elif pygame.mouse.get_pressed()[2]:
+                pass
+
+    pygame.quit()
+
+
+main(window, width)
